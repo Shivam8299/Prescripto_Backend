cc0208c731bbff1e0208212490ef84ab17406e97
Shivam8299 <shivammishra0381@gmail.com>
2025-01-03 09:16:23 +0530

    new commit

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDb from './config/mongoDB.js'
+import connectCloudinary from './config/cloudinary.js'
 
 const app = express()
 const port = process.env.PORT || 3000
+connectDb()
+connectCloudinary()
 
 // middlewares
 app.use(express.json());
 app.use(cors())
+
 
 app.get('/', (req,res)=>{
     res.send('hello from server side')
